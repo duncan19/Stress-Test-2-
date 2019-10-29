809a29d19f16f6399c722219b664cb24dd5bff6a
Student <example@example.com>
2019-10-29 14:23:23 -0700

    add if, else if, and else statement and add scoring for quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  var numberArray = []
+  var numberArray2 = []
   $("#formStress").submit(function(event) {
   event.preventDefault();
 
@@ -7,26 +9,41 @@ $(document).ready(function(){
   $("form").hide();
 
 $("input:checkbox[name=question1]:checked").each(function (){
-  var answer1 = $(this).val();
-  $('#stress-level').append(answer1 + "<br>");
+  var answer1 = parseInt($(this).val());
 
-
+  (numberArray).push(answer1);
+console.log(numberArray);
 });
 
 
 $("input:checkbox[name=question2]:checked").each(function (){
-  var answer2 = $(this).val();
-  $('#stress-level').append(answer2 + "<br>");
+  var answer2 = parseInt($(this).val());
+  (numberArray).push(answer2);
+    console.log(numberArray);
 
 
 });
 
 $("input:checkbox[name=question3]:checked").each(function (){
-  var answer3 = $(this).val();
-  $('#stress-strategy').append(answer3 + "<br>");
+  var answer3 = parseInt($(this).val());
+  (numberArray2).push(answer3);
+    console.log(numberArray2);
 
 
 });
+
+var score = numberArray.length - numberArray2.length;
+
+if (score >= 7) {
+  $("#high-stress").show();
+} else if (score >=4) {
+  $("#medium-stress").show();
+} else if (score >=0) {
+  $("#low-stress").show();
+} else {
+  $("#no-stress").show();
+}
+
 
 
 
