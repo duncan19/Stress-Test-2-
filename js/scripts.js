622d20d017e915fc033cb1365636338a146c4360
Student <example@example.com>
2019-10-29 13:52:27 -0700

    add HTML and initial js and jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
+  $("#formStress").submit(function(event) {
+  event.preventDefault();
+
+  $("#stress-level").show();
+  $("#stress-strategy").show();
+  $("form").hide();
+
+$("input:checkbox[name=question1]:checked").each(function (){
+  var answer1 = $(this).val();
+  $('#stress-level').append(answer1 + "<br>");
+
+
+});
+
+
+$("input:checkbox[name=question2]:checked").each(function (){
+  var answer2 = $(this).val();
+  $('#stress-level').append(answer2 + "<br>");
+
+
+});
+
+$("input:checkbox[name=question3]:checked").each(function (){
+  var answer3 = $(this).val();
+  $('#stress-strategy').append(answer3 + "<br>");
+
+
+});
+
+
+
+
+});
 
 });
